cbc11641aa9e4197b0a12b7aed64f35dc9df000a
Presian Kiradzhiev <107552667+pressy03@users.noreply.github.com>
2025-05-31 18:55:49 +0300

    Update AppointmentsPage.tsx

--- frontend/src/pages/AppointmentsPage.tsx
+++ frontend/src/pages/AppointmentsPage.tsx
@@ -124,7 +124,6 @@ export default function AppointmentsPage() {
               >
                 edit
               </button>
-
               )}
               {role === "doctor" && (
                 <button
